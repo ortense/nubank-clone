af24eeeec3c707dfa33137a75b0640eec7882acc
Marcus Ortense <marcus.ortense@outlook.com>
2020-01-24 14:54:24 -0300

    upgrade androidX

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/marcus.ortense/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/marcus.ortense/Projects/OC/nubank-clone"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/marcus.ortense/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
